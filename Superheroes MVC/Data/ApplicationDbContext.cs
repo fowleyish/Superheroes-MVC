e3be3c6b75f6de6cd8a9f35e6df3ddbb7f466766
Fowley <alexanderfowle@gmail.com>
2020-05-18 13:58:04 -0500

    Add project files.

---
+++ Superheroes MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Superheroes_MVC.Models;
+
+namespace Superheroes_MVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        DbSet<Superhero> Superheroes { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Superheroes MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Superheroes_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
